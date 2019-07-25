907117b8d6f6ce49535c803a6ac53b8a17b5c847
Chomtana <chomtana001@gmail.com>
2019-07-25 16:16:21 +0700

    fix bottom nav bug

--- src/layout/BottomNav.js
+++ src/layout/BottomNav.js
@@ -106,15 +106,90 @@ function Add_Microphone(props) {
         
         function doWebKit() {
           if (('webkitSpeechRecognition' in window)) {
-            var recognition = new webkitSpeechRecognition();
-            recognition.continuous = true;
-            recognition.interimResults = true;
+            var recognition = new window.webkitSpeechRecognition();
+            recognition.continuous = false;
+            recognition.interimResults = false;
             
             recognition.onstart = function() {
-              recognizing = true;
-              showInfo('info_speak_now');
-              start_img.src = '/intl/en/chrome/assets/common/images/content/mic-animate.gif';
+              //recognizing = true;
+              alert("start")
             };
+            
+            recognition.onerror = function(event) {
+              alert("error")
+            }
+            
+            /*recognition.onerror = function(event) {
+              if (event.error == 'no-speech') {
+                start_img.src = '/intl/en/chrome/assets/common/images/content/mic.gif';
+                showInfo('info_no_speech');
+                ignore_onend = true;
+              }
+              if (event.error == 'audio-capture') {
+                start_img.src = '/intl/en/chrome/assets/common/images/content/mic.gif';
+                showInfo('info_no_microphone');
+                ignore_onend = true;
+              }
+              if (event.error == 'not-allowed') {
+                if (event.timeStamp - start_timestamp < 100) {
+                  showInfo('info_blocked');
+                } else {
+                  showInfo('info_denied');
+                }
+                ignore_onend = true;
+              }
+            };
+            
+            recognition.onend = function() {
+              recognizing = false;
+              if (ignore_onend) {
+                return;
+              }
+              start_img.src = '/intl/en/chrome/assets/common/images/content/mic.gif';
+              if (!final_transcript) {
+                showInfo('info_start');
+                return;
+              }
+              showInfo('');
+              if (window.getSelection) {
+                window.getSelection().removeAllRanges();
+                var range = document.createRange();
+                range.selectNode(document.getElementById('final_span'));
+                window.getSelection().addRange(range);
+              }
+              if (create_email) {
+                create_email = false;
+                createEmail();
+              }
+            };*/
+            
+            recognition.onresult = function(event) {
+              console.log(event.results);
+              /*var interim_transcript = '';
+              if (typeof(event.results) == 'undefined') {
+                recognition.onend = null;
+                recognition.stop();
+                upgrade();
+                return;
+              }
+              for (var i = event.resultIndex; i < event.results.length; ++i) {
+                if (event.results[i].isFinal) {
+                  final_transcript += event.results[i][0].transcript;
+                } else {
+                  interim_transcript += event.results[i][0].transcript;
+                }
+              }
+              final_transcript = capitalize(final_transcript);
+              final_span.innerHTML = linebreak(final_transcript);
+              interim_span.innerHTML = linebreak(interim_transcript);
+              if (final_transcript || interim_transcript) {
+                showButtons('inline-block');
+              }*/
+            };
+            
+            recognition.lang = "th-TH";
+            recognition.start();
+            
           } else {
             alert("โปรดลง app หรือใช้ browser google chrome")
           }
@@ -149,7 +224,7 @@ function Add_Microphone(props) {
             }
           );
         } else {
-          doWebkit();
+          doWebKit();
         }
       }}
     >
