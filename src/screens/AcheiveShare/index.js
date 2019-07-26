0b878c92ac06b08922bcf70a97c285fa4df69cdc
Chomtana <chomtana001@gmail.com>
2019-07-26 12:49:14 +0700

    make 30

--- src/screens/AcheiveShare/index.js
+++ src/screens/AcheiveShare/index.js
@@ -9,7 +9,7 @@ export default function AcheiveShare(props) {
   return (
     <VBox>
       <div style={{textAlign: "center"}}>
-        <img src="https://www.goodnewsnetwork.org/wp-content/uploads/2015/12/Money-100-bills-CC-ree-pictures-of-money.jpg" width="50%"></img> 
+        <img src="./img/30.jpg" width="50%"></img> 
       </div>
       <div style={{textAlign: "center", fontSize: 20}}>
         {line_detail("name").isReady && <div>คุณ {line_detail("name")()}</div>}
