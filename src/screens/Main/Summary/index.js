05d0a4b29ba36d79ead72bc8a894f4b2d7257414
Chomtana <chomtana001@gmail.com>
2019-08-12 21:06:13 +0700

    remove head

--- src/screens/Main/Summary/index.js
+++ src/screens/Main/Summary/index.js
@@ -7,12 +7,9 @@ import DateSelector from "../../../component/DateSelector/DateSelector";
 import useStatePointer from "../../../pointer/useStatePointer";
 
 export default function Summary(props) {
-  const selectedMonth = useStatePointer(new Date().getMonth() + 1);
-  const selectedYear = useStatePointer(new Date().getYear());
   
   return (
     <VBox style={props.style}>
-      <DateSelector selectedMonth={selectedMonth} selectedYear={selectedYear}></DateSelector>
       <HBox style={{ backgroundColor: "white", padding: 10 }}>
         <ProfilePicture {...props} ratio={1} />
         <SummaryText {...props} ratio={5} />
